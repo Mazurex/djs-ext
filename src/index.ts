330141bd2eced1e307a9e44fb6542cff33652435
maz <ytmazurex@gmail.com>
2026-02-21 22:23:23 +0000

    Chore: Improve library entrypoint imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-export * from './types/BotEventListener'
+// Handlers
 export * from './handlers/events'
 
+// Types
+export * from './types/BotEventListener'
+export * from './types/Client'
+export * from './types/Command'
+
+// Essential
 export * from './Error'
+export * from './ExtendedClient'

2649723c3e5c7ad2cde32b9d49a4e2ca050caf4c
maz <ytmazurex@gmail.com>
2026-02-21 22:22:27 +0000

    Docs: Document DjsExtError error class

--- src/Error.ts
+++ src/Error.ts
@@ -2,6 +2,11 @@ export enum DjsExtErrorCodes {
     NoTokenProvided = 'No Token Provided',
 }
 
+/**
+ * Standard Djs-Ext error
+ * @param code Error code
+ * @param args Optional array of info
+ */
 export class DjsExtError extends Error {
     public constructor(code: DjsExtErrorCodes, args: unknown[] = []) {
         let format = `"${code}"`

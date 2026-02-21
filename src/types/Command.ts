74fe8f5db058b477c2153bf2d48d8d8d89be5207
maz <ytmazurex@gmail.com>
2026-02-21 22:17:47 +0000

    Chore: Merge prefix and slash command files

--- src/types/Command.ts
+++ src/types/Command.ts
@@ -1,10 +1,21 @@
 import {
     Awaitable,
     Interaction,
+    Message,
     SlashCommandBuilder,
     Snowflake,
 } from 'discord.js'
 import { ExtendedClient } from '../ExtendedClient'
+
+export interface PrefixCommand {
+    name: string
+    // TODO: Add more params
+    execute: (
+        client: ExtendedClient,
+        params: Record<string, unknown>,
+        message: Message
+    ) => Awaitable<void>
+}
 
 export interface SlashCommand {
     guilds?: Array<Snowflake>

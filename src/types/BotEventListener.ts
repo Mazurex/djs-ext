02c6db3454342aaa3be7dbbc807fa16efb19d26e
maz <ytmazurex@gmail.com>
2026-02-21 21:51:26 +0000

    Chore: Type indicate that an event execute method client parameter will use ExtendedClient instead of Client

--- src/types/BotEventListener.ts
+++ src/types/BotEventListener.ts
@@ -1,9 +1,13 @@
-import { Awaitable, Client, ClientEvents } from 'discord.js'
+import { Awaitable, ClientEvents } from 'discord.js'
+import { ExtendedClient } from '../ExtendedClient'
 
 export interface BotEventListener<T extends keyof ClientEvents> {
     type: T
     once?: boolean
-    execute: (client: Client, ...args: ClientEvents[T]) => Awaitable<void>
+    execute: (
+        client: ExtendedClient,
+        ...args: ClientEvents[T]
+    ) => Awaitable<void>
 }
 
 export const defineEvent = <T extends keyof ClientEvents>(

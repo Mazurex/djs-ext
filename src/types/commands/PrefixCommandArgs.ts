70c81fe18c2d6a6a2d845364bb05bf2a60212c00
maz <ytmazurex@gmail.com>
2026-02-23 01:22:31 +0000

    Docs: Add docstring to StringArg

--- src/types/commands/PrefixCommandArgs.ts
+++ src/types/commands/PrefixCommandArgs.ts
@@ -17,6 +17,31 @@ export abstract class GreedyArg<T, Name extends string> extends GenericArg<
     }
 }
 
+/**
+ * Create a new string argument for a prefix command.
+ *
+ * Usage:
+ * ```
+ * export const command: PrefixCommand = createPrefixCommand({
+ *    name: 'send',
+ *    args: [new StringArg('message').required().greedy()],
+ *    async execute(client, args, message) {
+ *        if (message.channel.isSendable())
+ *            await message.channel.send(args.message)
+ *    },
+ * })
+ * ```
+ *
+ * `StringArg()` only required a `name` parameter which will be used for identification and access in `execute(...) {}`.
+ *
+ * `.required(boolean)` determines whether this argument is required, if this is not set, the argument is `false` by default.
+ *
+ * `.greedy(boolean)` Makes this parameter greedy, meaning it consumes the rest of the command, meaning:
+ *
+ * "!message this is a message" will set the "message" param to "this is a message".
+ *
+ * If greedy is `false` in the above scenario, an invalid arg count error will be produced.
+ */
 export class StringArg<Name extends string> extends GreedyArg<string, Name> {
     deserialize(input: string): string {
         return input

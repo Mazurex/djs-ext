e782dc5f8fc3ecbe3e60fd6d284c8f73e052ec50
maz <ytmazurex@gmail.com>
2026-04-12 21:28:56 +0100

    Chore: Clean up example bot index file

--- examples/discord-bot/src/index.ts
+++ examples/discord-bot/src/index.ts
@@ -1,28 +1,21 @@
 import {
-    DefaultClientIntents,
+    BotEventListener,
     ExtendedClient,
-    registerGenericFromPath,
+    fetchModuleInstances,
+    ModulePredicate,
+    isEventListener,
 } from '../../../dist/index.js'
 import 'dotenv/config'
 
 import { fileURLToPath } from 'node:url'
-import { dirname } from 'node:path'
+import path, { dirname } from 'node:path'
 import { GatewayIntentBits } from 'discord.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-const client = new ExtendedClient({
-    intents: [...DefaultClientIntents, GatewayIntentBits.MessageContent],
-})
-
-await registerGenericFromPath(client, './events', __dirname, 'event')
-await registerGenericFromPath(client, './prefix_commands', __dirname, 'prefix')
-await registerGenericFromPath(client, './slash_commands', __dirname, 'slash')
+const client = new ExtendedClient()
 
 console.debug('Starting bot brrrrrrrrrrrrrrrrrrrrr')
 
 await client.login(process.env.BOT_TOKEN)
-
-console.log(client.prefixCommands)
-console.log(client.slashCommands)

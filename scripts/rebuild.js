d4dcba3ab314bfbbbdd7e41f2081e9b301481c8a
maz <ytmazurex@gmail.com>
2026-04-15 12:54:01 +0100

    Feat: Improve the run example bot script to use js instead of sh, adding a script into the root package.json to run this easily

---
+++ scripts/rebuild.js
@@ -0,0 +1,23 @@
+/**
+ *  Run this script to delete all dist/ directories and rebuild them via `pnpm build`
+ */
+
+const { execSync } = require('node:child_process')
+const { rmSync, readdirSync } = require('node:fs')
+const { resolve, join } = require('node:path')
+
+readdirSync('packages/').forEach((pkg) => {
+    rmSync(resolve(join('packages', pkg, 'dist')), {
+        recursive: true,
+        force: true,
+    })
+})
+
+readdirSync('examples/').forEach((pkg) => {
+    rmSync(resolve(join('examples', pkg, 'dist')), {
+        recursive: true,
+        force: true,
+    })
+})
+
+execSync('pnpm build', { stdio: 'inherit' })

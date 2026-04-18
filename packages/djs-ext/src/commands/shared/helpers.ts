928f6158f97a4ff1b4481a3f203746a988a5afcf
maz <ytmazurex@gmail.com>
2026-04-18 23:28:34 +0100

    Docs: add docstring arrayAppend method

--- packages/djs-ext/src/commands/shared/helpers.ts
+++ packages/djs-ext/src/commands/shared/helpers.ts
@@ -1,3 +1,13 @@
+/**
+ * This function simplifies array append logic.
+ *
+ * If `value`:
+ * - is an array, it is joined onto `array`
+ * - is **not** an array, it is appended onto `array`
+ *
+ * @param array The array to append onto
+ * @param value The value to append onto the array
+ */
 export function arrayAppend<T>(array: T[], value: T) {
     if (Array.isArray(value)) array.push(...value)
     else array.push(value)
